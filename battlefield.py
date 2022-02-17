67f1b361ceee5d900578990f234a7c7783b0b113
ARyback <adam.ryback@gmail.com>
2022-02-16 18:08:20 -0600

    This commit records most of the requirements which need to be met. However, the project still needs work.

--- battlefield.py
+++ battlefield.py
@@ -9,8 +9,7 @@ class Battlefield:
 
     def run_game(self):
         self.display_welcome()
-        self.battle(1,2)
-        self.display_winners()
+        self.battle(0,0)
         pass
 
     def display_welcome(self):
@@ -19,12 +18,23 @@ class Battlefield:
         pass
 
     def battle(self, dinosaur_index, robot_index):
-        while (self.fleet.robots[robot_index].health > 0 or self.herd.dinosaurs[dinosaur_index].health > 0):
+        while (self.fleet.robots[robot_index].health > 0 and self.herd.dinosaurs[dinosaur_index].health > 0):
             self.dino_turn(dinosaur_index, robot_index)
             self.robo_turn(robot_index, dinosaur_index)
+            if self.fleet.robots[robot_index].health < 0:
+                self.fleet.robots[robot_index].health
+            if self.herd.dinosaurs[dinosaur_index].health > 0:
+                self.herd.dinosaurs[dinosaur_index].health
             print(f"The health of the {self.herd.dinosaurs[dinosaur_index].name} is {self.herd.dinosaurs[dinosaur_index].health}.")
             print(f"The health of the {self.fleet.robots[robot_index].name} is {self.fleet.robots[robot_index].health}.")
-        pass
+            if (self.fleet.robots[robot_index].health <= 0 or self.herd.dinosaurs[dinosaur_index].health <= 0):
+                if self.fleet.robots[robot_index].health == 0:
+                    self.display_winners(self.herd.dinosaurs[dinosaur_index].name)
+                elif self.herd.dinosaurs[dinosaur_index].health == 0:
+                    self.display_winners(self.fleet.robots[robot_index].name)
+                else: 
+                    print("There was an error")
+            pass
 
     def dino_turn(self, dinosaur_index, robot_index):
         self.herd.dinosaurs[dinosaur_index].attack(self.fleet.robots[robot_index])
@@ -40,7 +50,7 @@ class Battlefield:
     def show_robo_opponent_options(self):
         pass
 
-    def display_winners(self):
-        print(f"Winners are always dinosaurs!")
+    def display_winners(self, winner):
+        print(f"The winner is {winner}!")
         pass
 
